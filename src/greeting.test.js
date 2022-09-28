713412d7c17270ac732e6f381ffaabdaca3d2afb
Diego Becco <diego.becco@etermax.com.uy>
2022-09-28 10:21:33 -0300

    When player one scores, is 15-Love

--- src/greeting.test.js
+++ src/greeting.test.js
@@ -2,9 +2,15 @@ const greeting = require('./greeting')
 const expect = require("expect");
 
 class TennisGame {
+    playerOneScore=0;
 
     getScore() {
+        if(this.playerOneScore>0) return "15-Love"
         return "Love-all";
+    }
+
+    wonPoint(player) {
+        this.playerOneScore ++;
     }
 }
 
@@ -13,6 +19,10 @@ test('A new game starts with Love-all', () => {
     expect(game.getScore()).toBe("Love-all");
 });
 
-
+test('When player one scores, score is 15-Love', () => {
+    const game = new TennisGame();
+    game.wonPoint("playerOne")
+    expect(game.getScore()).toBe("15-Love");
+});
 
 
