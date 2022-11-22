632303258fcafa656458413326c324d19085f4fa
regi <regina.maite.at@gmail.com>
2022-11-22 20:40:05 -0300

    TennisGame start

--- src/greeting.test.js
+++ src/greeting.test.js
@@ -5,4 +5,41 @@ test('works', () => {
     expect(greeting()).toBe("Hello");
 });
 
+class TennisGame{
+    scorePlayerOne = 0
+    scorePlayerTwo = 0
+    constructor(){
 
+    }
+    getScore(){
+        if (this.scorePlayerOne == 1 && this.scorePlayerTwo == 0){
+            return "15-Love"
+        }
+        return "Love-all"
+    }
+    wonPoint(player){
+      if (player == "player1"){
+        this.scorePlayerOne++
+      }
+      else{
+        this.scorePlayerTwo++
+      }
+    }
+}
+
+test('start-game',() =>{
+    const game = new TennisGame()
+    expect(game.getScore()).toBe("Love-all");
+})
+
+test('player1-score',() =>{
+    const game = new TennisGame()
+    game.wonPoint('player1')
+    expect(game.getScore()).toBe("15-Love");
+})
+
+test('player1-score',() =>{
+    const game = new TennisGame()
+    game.wonPoint('player1')
+    expect(game.getScore()).toBe("15-Love");
+})
