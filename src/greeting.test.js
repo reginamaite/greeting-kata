ec5b49d69426bd40c1dd3a2feb629447a287b7f4
BadSh4dow <guidocabrera711@gmail.com>
2022-11-22 21:47:18 -0300

    vamo arriba la puta madre

--- src/greeting.test.js
+++ src/greeting.test.js
@@ -6,40 +6,60 @@ test('works', () => {
 });
 
 class TennisGame{
-    scorePlayerOne = 0
-    scorePlayerTwo = 0
-    constructor(){
+    constructor(scorePlayerOne,scorePlayerTwo){
+    this.scorePlayerOne = scorePlayerOne
+    this.scorePlayerTwo = scorePlayerTwo
 
     }
+
     getScore(){
         if (this.scorePlayerOne == 1 && this.scorePlayerTwo == 0){
             return "15-Love"
+        }else if(this.scorePlayerOne == 0 && this.scorePlayerTwo == 1){
+            return "Love-15"
+        }
+        if (this.scorePlayerOne == 1 && this.scorePlayerTwo == 1){
+            return "15-all"
+        }
+        if (this.scorePlayerOne == 2 && this.scorePlayerTwo == 1){
+            return "30-15"
         }
         return "Love-all"
     }
     wonPoint(player){
       if (player == "player1"){
         this.scorePlayerOne++
-      }
-      else{
+      }else{
         this.scorePlayerTwo++
       }
     }
 }
 
 test('start-game',() =>{
-    const game = new TennisGame()
+    const game = new TennisGame(0,0)
     expect(game.getScore()).toBe("Love-all");
 })
 
 test('player1-score',() =>{
-    const game = new TennisGame()
+    const game = new TennisGame(0,0)
     game.wonPoint('player1')
     expect(game.getScore()).toBe("15-Love");
 })
 
-test('player1-score',() =>{
-    const game = new TennisGame()
-    game.wonPoint('player1')
-    expect(game.getScore()).toBe("15-Love");
+test('player2-score',() =>{
+    const game = new TennisGame(0,0)
+    game.wonPoint('player2')
+    expect(game.getScore()).toBe("Love-15");
 })
+
+test('15-all',() =>{
+    const game = new TennisGame(1,0)
+    game.wonPoint('player2')
+    expect(game.getScore()).toBe("15-all");
+})
+test('player1-score-again',() =>{
+    const game = new TennisGame(1,1)
+    game.wonPoint('player1')
+    expect(game.getScore()).toBe("30-15");
+})
+
